2c14cbc19a28e75be0b032e22baf516b04559826
Vincent Siles <vsiles@fb.com>
2019-08-07 15:33:53 +0200

    move load instuctions into sub folder

---
+++ src/cpu/load/mod.rs
@@ -0,0 +1,14 @@
+#[macro_use]
+mod load_imm;
+#[macro_use]
+mod load_zp;
+#[macro_use]
+mod load_zp_reg;
+#[macro_use]
+mod load_abs;
+#[macro_use]
+mod load_abs_reg;
+#[macro_use]
+mod load_ndx_ind;
+#[macro_use]
+mod load_ind_ndx;
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -5,20 +5,7 @@ pub mod nop;
 #[macro_use]
 mod macros;
 #[macro_use]
-mod load_imm;
-#[macro_use]
-mod load_zp;
-#[macro_use]
-mod load_zp_reg;
-#[macro_use]
-mod load_abs;
-#[macro_use]
-mod load_abs_reg;
-#[macro_use]
-mod load_ndx_ind;
-#[macro_use]
-mod load_ind_ndx;
-
+mod load;
 
 pub use cpu::*;
 pub use opcode::OpCode;
