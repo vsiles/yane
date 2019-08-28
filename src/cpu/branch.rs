e2c22e2e59771c748a782fdb34640768f0af8839
Vincent Siles <vsiles@fb.com>
2019-08-28 13:15:03 +0200

    fixing branch calculation (signed vs unsigned)

--- src/cpu/branch.rs
+++ src/cpu/branch.rs
@@ -1,9 +1,8 @@
 macro_rules! declare_branch {
     ($name:ident, $flag:ident, $val:expr, $mnemo:ident) => {
         pub struct $name {
-            imm: u8,
+            imm: i8,
             state: usize,
-            carry: bool,
         }
 
         impl OpCode for $name {
@@ -11,29 +10,45 @@ macro_rules! declare_branch {
                 $name {
                     imm: 0,
                     state: 0,
-                    carry: false,
                 }
             }
+      //   5!    PC      R  Fetch opcode of next instruction,
+      //                    increment PC.
+
+      //  Notes: The opcode fetch of the next instruction is included to
+      //         this diagram for illustration purposes. When determining
+      //         real execution times, remember to subtract the last
+      //         cycle.
+
+      //         * The high byte of Program Counter (PCH) may be invalid
+      //           at this time, i.e. it may be smaller or bigger by $100.
+
+      //         + If branch is taken, this cycle will be executed.
+
+      //         ! If branch occurs to different page, this cycle will be
+      //           executed.
 
             fn decode(&mut self, cpu: &mut Cpu) -> bool {
                 if self.state == 0 {
-                    self.imm = cpu.read_from_pc();
-                    self.state = self.state + 1;
+                    //   2     PC      R  fetch operand, increment PC
+                    self.imm = cpu.read_from_pc() as i8;
+                    self.state = 1;
                     false
                 } else if self.state == 1 {
-                    self.state = self.state + 1;
+                    //   3     PC      R  Fetch opcode of next instruction,
+                    //                    If branch is taken, add operand to PCL.
+                    //                    Otherwise increment PC.
+                    self.state = 2;
                     if cpu.flags.$flag == $val {
-                        let pc = cpu.pc.overflowing_add(self.imm as u16);
-                        cpu.pc = pc.0;
-                        self.carry = pc.1;
                         false
                     } else {
                         true
                     }
                 } else {
-                    if self.carry {
-                        cpu.pc = cpu.pc + 0x100
-                    }
+                    //   4+    PC*     R  Fetch opcode of next instruction.
+                    //                    Fix PCH. If it did not change, increment PC.
+                    let pc : i32 = cpu.pc as i32;
+                    cpu.pc = (pc + (self.imm as i32)) as u16;
                     true
                 }
             }
@@ -41,15 +56,15 @@ macro_rules! declare_branch {
             fn log(&self, cpu: &Cpu) {
                 let pc = cpu.pc - 1;
                 let code = cpu.mem.get(pc);
-                let imm = cpu.mem.get(pc + 1);
-                let addr = (pc as usize) + 2 + (imm as usize);
+                let imm = cpu.mem.get(pc + 1) as i8;
+                let addr : i32 = (pc as i32) + (imm as i32) + 2;
                 print!(
                     "{:04X}  {:02X} {:02X}     {} ${:04X}",
                     pc,
                     code,
                     imm,
                     stringify!($mnemo),
-                    addr
+                    addr as u16
                 );
                 print!("{: <23}{}", "", cpu)
             }
