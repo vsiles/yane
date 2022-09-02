040413a8c45f3258c7226ed850111d181bf3ab45
Vincent Siles <vincent.siles@ens-lyon.org>
2022-09-02 08:30:49 +0200

    first few instrs

--- src/cpu.rs
+++ src/cpu.rs
@@ -76,6 +76,7 @@ impl From<u8> for Status {
 
 pub struct Cpu {
     pub a: u8,
+    pub x: u8,
     pub ps: Status,
     pub pc: u16,
 }
@@ -84,12 +85,168 @@ impl Cpu {
     pub fn new() -> Self {
         Cpu {
             a: 0,
+            x: 0,
             ps: Status::new(),
             pc: 0,
         }
     }
 
-    pub fn interpret(&mut self, _program: Vec<u8>) {
-        todo!("")
+    fn update_zero_and_negative(&mut self, result: u8) {
+        self.ps.set(Zero, result == 0);
+        self.ps.set(Negative, (result & (1 << 7)) != 0)
+    }
+
+    fn lda(&mut self, value: u8) {
+        // Cycles 2
+        self.a = value;
+        self.update_zero_and_negative(self.a);
+    }
+
+    fn tax(&mut self) {
+        self.x = self.a;
+        self.update_zero_and_negative(self.x);
+    }
+
+    fn inx(&mut self) {
+        let (x, _) = self.x.overflowing_add(1);
+        self.x = x;
+        self.update_zero_and_negative(self.x);
+    }
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        self.pc = 0;
+
+        loop {
+            let opcode = program[self.pc as usize];
+            self.pc += 1;
+
+            match opcode {
+                // BRK
+                0x00 => return,
+                // LDA
+                0xA9 => {
+                    // 2 Cycles
+                    let a = program[self.pc as usize];
+                    self.pc += 1;
+                    self.lda(a);
+                }
+                // TAX : 2 Cycles
+                0xAA => self.tax(),
+                // INX : 2 Cycles
+                0xE8 => self.inx(),
+                _ => todo!(),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_0xa9_lda_imm() {
+        let mut cpu = Cpu::new();
+        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.a, 0x05);
+        assert!(cpu.ps.zero == false);
+        assert!(cpu.ps.negative == false);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = Cpu::new();
+        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        assert_eq!(cpu.a, 0x00);
+        assert!(cpu.ps.zero == true);
+        assert!(cpu.ps.negative == false);
+    }
+
+    #[test]
+    fn test_0xa9_lda_neg_flag() {
+        let mut cpu = Cpu::new();
+        cpu.interpret(vec![0xa9, 0xf0, 0x00]);
+        assert_eq!(cpu.a, 0xf0);
+        assert!(cpu.ps.zero == false);
+        assert!(cpu.ps.negative == true);
+    }
+
+    #[test]
+    fn test_0xaa_tax() {
+        let mut cpu = Cpu::new();
+        cpu.a = 0x0b;
+        cpu.interpret(vec![0xaa, 0x00]);
+        assert_eq!(cpu.a, 0x0b);
+        assert_eq!(cpu.x, 0x0b);
+        assert!(cpu.ps.zero == false);
+        assert!(cpu.ps.negative == false);
+    }
+
+    #[test]
+    fn test_0xaa_tax_zero_flag() {
+        let mut cpu = Cpu::new();
+        cpu.x = 0x0b;
+        cpu.interpret(vec![0xaa, 0x00]);
+        assert_eq!(cpu.x, 0x00);
+        assert!(cpu.ps.zero == true);
+        assert!(cpu.ps.negative == false);
+    }
+
+    #[test]
+    fn test_0xaa_tax_negative_flag() {
+        let mut cpu = Cpu::new();
+        cpu.x = 0x0b;
+        cpu.a = 0xf0;
+        cpu.interpret(vec![0xaa, 0x00]);
+        assert_eq!(cpu.x, 0xf0);
+        assert!(cpu.ps.zero == false);
+        assert!(cpu.ps.negative == true);
+    }
+
+    #[test]
+    fn test_0xe8_inx() {
+        let mut cpu = Cpu::new();
+        cpu.x = 0x0a;
+        cpu.interpret(vec![0xe8, 0x00]);
+        assert_eq!(cpu.x, 0x0b);
+        assert!(cpu.ps.zero == false);
+        assert!(cpu.ps.negative == false);
+    }
+
+    #[test]
+    fn test_0xe8_inx_zero_flag() {
+        let mut cpu = Cpu::new();
+        cpu.x = 0xff;
+        cpu.interpret(vec![0xe8, 0x00]);
+        assert_eq!(cpu.x, 0x00);
+        assert!(cpu.ps.zero == true);
+        assert!(cpu.ps.negative == false);
+    }
+
+    #[test]
+    fn test_0xe8_inx_negative_flag() {
+        let mut cpu = Cpu::new();
+        cpu.x = 0xf0;
+        cpu.interpret(vec![0xe8, 0x00]);
+        assert_eq!(cpu.x, 0xf1);
+        assert!(cpu.ps.zero == false);
+        assert!(cpu.ps.negative == true);
+    }
+
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = Cpu::new();
+        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        assert_eq!(cpu.x, 0xc1)
+    }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = Cpu::new();
+        cpu.x = 0xff;
+        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+
+        assert_eq!(cpu.x, 1)
     }
 }
